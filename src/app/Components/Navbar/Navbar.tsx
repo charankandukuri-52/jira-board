144bc5441d6529068762cbb3426c3bda99f14208
charan52705 <charan.kandukuri.527@gmail.com>
2024-08-02 11:34:09 +0530

    fix typos

--- src/app/Components/Navbar/Navbar.tsx
+++ src/app/Components/Navbar/Navbar.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import '../Cosmetics/Navbar.css'
 import NavItems from '../Data/NavItems'
 import logo from '../../../assets/Atlassian_Logo.png'
-
 import { navLists } from '../Data/NavList';
 
 function Navbar() {
@@ -32,7 +31,7 @@ function Navbar() {
                         <button className='free-btn'> <h2>Get it  Free</h2></button>
                     </div>
                     <div className='search-icon'>
-                        <svg width="23" height="25" viewBox="0 0 23 25" fill="none" xmlns="http://www.w3.org/2000/svg" className="_1bsb1tcg _4t3i1tcg _1pbykb7n _1otx14rb _1tindoyu"><path d="M17.0918 18.5623L20.2497 21.7201" stroke="#101214" stroke-width="2.75" stroke-linecap="round"></path><path d="M10.8417 19.1316C15.289 19.1316 18.8943 15.5263 18.8943 11.079C18.8943 6.63165 15.289 3.02637 10.8417 3.02637C6.39435 3.02637 2.78906 6.63165 2.78906 11.079C2.78906 15.5263 6.39435 19.1316 10.8417 19.1316Z" stroke="#101214" stroke-width="2.25"></path></svg>
+                        <svg width="23" height="25" viewBox="0 0 23 25" fill="none" xmlns="http://www.w3.org/2000/svg" className="_1bsb1tcg _4t3i1tcg _1pbykb7n _1otx14rb _1tindoyu"><path d="M17.0918 18.5623L20.2497 21.7201" stroke="#101214" strokeWidth="2.75" strokeLinecap="round"></path><path d="M10.8417 19.1316C15.289 19.1316 18.8943 15.5263 18.8943 11.079C18.8943 6.63165 15.289 3.02637 10.8417 3.02637C6.39435 3.02637 2.78906 6.63165 2.78906 11.079C2.78906 15.5263 6.39435 19.1316 10.8417 19.1316Z" stroke="#101214" strokeWidth="2.25"></path></svg>
                     </div>
                     <div className='user-login'>
                         <div className='divider'></div>
